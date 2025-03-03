c973e810e1c3ca12b98af375db3de6eff8e3446c
BrunoVieiraL <bruno.leo516@hotmail.com>
2025-03-03 13:28:15 -0300

    only flutter build web files

--- lib/ui/core/theme/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract final class AppColors {
-  static final Color blue = Color(0xFF1565C0);
-  static final Color red = Color(0xFFB81114); 
-  static final Color orange = Color(0xFFD0670B);
-  static final Color purple = Color(0xFF622E9F);
-}
